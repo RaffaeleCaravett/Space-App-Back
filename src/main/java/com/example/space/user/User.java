9add0f0cca2ab3c906fad5f685d7b5445ca8c31e
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-25 10:27:39 +0200

    implemented userDetails in user class and created role enum

--- src/main/java/com/example/space/user/User.java
+++ src/main/java/com/example/space/user/User.java
@@ -1,12 +1,19 @@
 package com.example.space.user;
 
+import com.example.space.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -15,4 +22,36 @@ public class User {
     private String nome;
     private String cognome;
     private int eta;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
