0120ac9f658f9336b9f4baeebee18fc09634e707
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-26 09:54:39 +0200

    created prenotazione repository

---
+++ src/main/java/com/example/space/prenotazioni/PrenotazioneRepository.java
@@ -0,0 +1,8 @@
+package com.example.space.prenotazioni;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione,Long> {
+}
