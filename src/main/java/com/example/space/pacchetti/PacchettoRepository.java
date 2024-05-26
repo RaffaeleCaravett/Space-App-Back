6c422f0fff419f2a9c7d33be97ded7cb0cb10c15
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-26 09:57:57 +0200

    starting add method to repo

--- src/main/java/com/example/space/pacchetti/PacchettoRepository.java
+++ src/main/java/com/example/space/pacchetti/PacchettoRepository.java
@@ -3,6 +3,9 @@ package com.example.space.pacchetti;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PacchettoRepository extends JpaRepository<Pacchetto,Long> {
+    List<Pacchetto> findByPiante_Id(long id);
 }
