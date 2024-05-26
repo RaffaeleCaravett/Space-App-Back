344336d2f829c0733ced805cbe743a33e9316644
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-26 09:55:24 +0200

    added find by pacchetto_id method in rep

--- src/main/java/com/example/space/prenotazioni/PrenotazioneRepository.java
+++ src/main/java/com/example/space/prenotazioni/PrenotazioneRepository.java
@@ -3,6 +3,9 @@ package com.example.space.prenotazioni;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione,Long> {
+    List<Prenotazione> findByPacchetto_Id(long id);
 }
