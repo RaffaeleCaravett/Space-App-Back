abca1fbdf7f29462b08da654c6785d8438375d65
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-26 10:06:11 +0200

    added another method

--- src/main/java/com/example/space/pacchetti/PacchettoRepository.java
+++ src/main/java/com/example/space/pacchetti/PacchettoRepository.java
@@ -3,11 +3,14 @@ package com.example.space.pacchetti;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface PacchettoRepository extends JpaRepository<Pacchetto,Long> {
-    List<Pacchetto> findByPiante_Id(long id);
+    List<Pacchetto> findByPianeta_Id(long id);
 
     List<Pacchetto> findByPrezzoBetween(double first,double second);
+
+    List<Pacchetto> findByDaGreaterThanEqualAndALessThanEqual(LocalDate first, LocalDate second);
 }
