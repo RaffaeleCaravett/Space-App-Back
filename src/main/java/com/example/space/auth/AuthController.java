948493bb6d97c46e38308d3e9e95f5d1f8e7a3a5
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-27 17:52:52 +0200

    created auth controller

--- src/main/java/com/example/space/auth/AuthController.java
+++ src/main/java/com/example/space/auth/AuthController.java
@@ -1,4 +1,77 @@
 package com.example.space.auth;
 
+import com.example.space.exceptions.BadRequestException;
+import com.example.space.payloads.entities.Token;
+import com.example.space.payloads.entities.UserLoginDTO;
+import com.example.space.payloads.entities.UserLoginSuccessDTO;
+import com.example.space.payloads.entities.UserRegistrationDTO;
+import com.example.space.security.JWTTools;
+import com.example.space.user.User;
+import com.example.space.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JWTTools jwtTools;
+
+    @Autowired
+    private UserService utenteService;
+
+
+
+    @PostMapping("/login")
+    public UserLoginSuccessDTO login(@RequestBody UserLoginDTO body) throws Exception {
+
+        try{
+            return new UserLoginSuccessDTO(authService.authenticateUser(body));
+        }catch (Exception e){
+            throw  new BadRequestException(e.getMessage());
+        }
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED) // <-- 201
+    public User saveUser(@RequestBody @Validated UserRegistrationDTO body, BindingResult validation){
+        if(validation.hasErrors()){
+            throw new BadRequestException(validation.getAllErrors());
+        } else {
+            try {
+                return authService.registerUser(body);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @GetMapping("/{token}")
+    @ResponseStatus(HttpStatus.OK)
+    public User verifyToken(@PathVariable String token){
+        return jwtTools.verifyToken(token);
+    }
+    @GetMapping("/refreshToken/{refreshToken}")
+    @ResponseStatus(HttpStatus.OK)
+    public Token verifyRefreshToken(@PathVariable String refreshToken){
+        return jwtTools.verifyeRefreshToken(refreshToken);
+    }
+
+
+    @GetMapping("/user")
+    public Page<User> getUser(@RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "5") int size,
+                              @RequestParam(defaultValue = "id") String orderBy){
+        return authService.getUtenti(page, size, orderBy);
+    }
+
 }
