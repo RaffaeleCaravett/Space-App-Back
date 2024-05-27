9c2794e85ce6a677251641995e57074a534e388d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-27 16:47:22 +0200

    created error handlers and errors DTO

---
+++ src/main/java/com/example/space/payloads/errors/ErrorsDTO.java
@@ -0,0 +1,20 @@
+package com.example.space.payloads.errors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorsDTO {
+
+    private String message;
+    private Date date;
+
+}
