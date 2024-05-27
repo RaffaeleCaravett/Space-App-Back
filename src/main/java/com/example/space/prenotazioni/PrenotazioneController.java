7d8161288004f8e7ce4e4f94695578558d12c9ad
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-27 11:27:12 +0200

    done prenotazione controller

--- src/main/java/com/example/space/prenotazioni/PrenotazioneController.java
+++ src/main/java/com/example/space/prenotazioni/PrenotazioneController.java
@@ -8,6 +8,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
 @RequestMapping("/prenotazioni")
 public class PrenotazioneController {
@@ -32,5 +35,25 @@ public Prenotazione save (@RequestBody @Validated PrenotazioneDTO prenotazioneDT
     public Page<Prenotazione> getByAll(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String orderBy){
         return prenotazioneService.getAllPaginated(page,size,orderBy);
     }
+    @DeleteMapping("/{id}")
+    public boolean deleteById(@PathVariable long id){
+    return prenotazioneService.deleteById(id);
+    }
+    @PutMapping("/{id}")
+    public Prenotazione put (@PathVariable long id,@RequestBody @Validated PrenotazioneDTO prenotazioneDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return prenotazioneService.putById(id,prenotazioneDTO);
+    }
+    @GetMapping("pacchetto/{id}")
+    public List<Prenotazione> getByPacchettoId(@PathVariable long id){
+        return prenotazioneService.findByPacchettoId(id);
+    }
 
+    @GetMapping("date/{id}")
+    public List<Prenotazione> getByDate( @RequestParam int year, @RequestParam int month, @RequestParam int day,@PathVariable long id){
+        LocalDate localDate = LocalDate.of(year,month,day);
+        return prenotazioneService.findByDateAndUser(localDate,id);
+    }
 }
