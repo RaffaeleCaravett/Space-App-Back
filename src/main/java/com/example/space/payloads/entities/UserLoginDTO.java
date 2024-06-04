77f802d6f776bfa84f57eb080e3af9e9c1cd6d0b
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-04 11:23:33 +0200

    done login

--- src/main/java/com/example/space/payloads/entities/UserLoginDTO.java
+++ src/main/java/com/example/space/payloads/entities/UserLoginDTO.java
@@ -1,11 +1,15 @@
 package com.example.space.payloads.entities;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 public record UserLoginDTO(
         @NotEmpty(message = "Email necessaria")
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$", message = "Prova ad inserira una email tipo : 'a@a.com'")
         String email,
         @NotEmpty(message = "Password necessaria")
+        @Length(min = 6,message = "Lunghezza minima della password : sei caratteri.")
         String password
 ) {
 }
